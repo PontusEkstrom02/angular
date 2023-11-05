3c82174a4886bd415837c3b1116e9b5bede3ed5a
PontusEkstrom02 <113009598+PontusEkstrom02@users.noreply.github.com>
2023-11-05 14:22:08 +0100

    if statements in conponents

--- FirstTest/src/app/test-component/test-component.component.ts
+++ FirstTest/src/app/test-component/test-component.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./test-component.component.sass']
 })
 export class TestComponentComponent {
+  test = 'IDK WHY BUT COOL IGUESS';
+  fontColor = 'blue';
+  testId = 1;
+  canClick = true;
+  messageEx1 = 'Hello, World';
+ 
+  sayMessage() {
+    alert(this.message);
+  }
 
+  message = "I'm read only!";
+  canEdit = false;
+
+  onEditClick() {
+    this.canEdit = !this.canEdit;
+    if (this.canEdit) {
+      this.message = 'You can edit me!';
+    } else {
+      this.message = "I'm read only!";
+    }
+  }
 }
